5e60056c02b315dcc9042cff6560993432b1b0ab
root <ashalekeen@gmail.com>
2023-09-04 11:54:09 -0400

    Add typst exclusion for tree sitter

--- lua/plugin_config/treesitter.lua
+++ lua/plugin_config/treesitter.lua
@@ -1,11 +1,19 @@
 require('nvim-treesitter.install').prefer_git = true
 
+local disable_function = function(lang)
+		local buf_name = vim.fn.expand("%")
+		if string.find(buff_name, ".typ") then
+			return true
+		end
+	end
+
 require('nvim-treesitter.configs').setup({
-    ensure_installed = {"jsdoc", "css", "javascript", "kotlin", "latex", "lua", "make", "markdown", "markdown_inline", "java", "svelte", "typescript", "rust", "sql", "cpp", "c", "cmake"},
+    ensure_installed = {"jsdoc", "css", "javascript", "kotlin", "latex", "lua", "make", "markdown", "markdown_inline", "java", "svelte", "typescript", "rust", "sql", "cpp", "c", "cmake", },
     sync_install = true,
     auto_install = true,
     highlight = {
         enable = true,
+	disable = disable_function
     },
     indent = { enable = true, disable = {"python", "css"} },
 })
