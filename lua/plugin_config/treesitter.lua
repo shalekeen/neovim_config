c025506bd06adb08b50a8d1cd6c50acbe8a56893
root <ashalekeen@gmail.com>
2023-09-10 17:38:12 -0400

    Update treesitter conf

--- lua/plugin_config/treesitter.lua
+++ lua/plugin_config/treesitter.lua
@@ -2,13 +2,16 @@ require('nvim-treesitter.install').prefer_git = true
 
 local disable_function = function(lang)
 		local buf_name = vim.fn.expand("%")
-		if string.find(buff_name, ".typ") then
+		if string.find(buf_name, ".typ") then
 			return true
 		end
 	end
 
 require('nvim-treesitter.configs').setup({
-    ensure_installed = {"jsdoc", "css", "javascript", "kotlin", "latex", "lua", "make", "markdown", "markdown_inline", "java", "svelte", "typescript", "rust", "sql", "cpp", "c", "cmake", },
+    ensure_installed = {"jsdoc", "css", "javascript", "kotlin", "latex",
+    			"lua", "make", "markdown", "markdown_inline", 
+			"java", "svelte", "typescript", "rust", "sql", 
+			"cpp", "c", "cmake", "vim"},
     sync_install = true,
     auto_install = true,
     highlight = {
