ba42486623f7e6c6c5df2726414156543d35f396
Adib Shalekeen <ashalekeen@gmail.com>
2025-02-23 19:45:45 -0500

    Add support for flow

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,3 +1,7 @@
 return {
     url = "https://github.com/neovim/nvim-lspconfig",
+    config = function()
+        local lspconfig = require('lspconfig')
+        lspconfig.flow.setup({})
+    end
 }
